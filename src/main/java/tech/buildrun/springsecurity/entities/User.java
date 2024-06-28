301009e956afdbe37abe9982f85bc761f50f4183
Lucas Dantas Nogueira <lucas.dantas.nogueira@gmail.com>
2024-06-28 15:12:43 -0300

    first commit

---
+++ src/main/java/tech/buildrun/springsecurity/entities/User.java
@@ -0,0 +1,28 @@
+package tech.buildrun.springsecurity.entities;
+
+import java.util.Set;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "s_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "user_id")
+    private UUID userId;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    private Set<Role> roles;
+}
