3bf9e2fa72b972984db565705235836e93c72c90
Lucas Dantas Nogueira <lucas.dantas.nogueira@gmail.com>
2024-06-29 15:30:29 -0300

    feat: add dto for user request and response

---
+++ src/main/java/tech/buildrun/springsecurity/dto/user/UserRequest.java
@@ -0,0 +1,4 @@
+package tech.buildrun.springsecurity.dto.user;
+
+public record UserRequest(String username, String password) {
+}
---
+++ src/main/java/tech/buildrun/springsecurity/dto/user/UserResponse.java
@@ -0,0 +1,4 @@
+package tech.buildrun.springsecurity.dto.user;
+
+public record UserResponse(String username) {
+}
